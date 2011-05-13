4d2bfc34e3aecf0e61d993db0c16f647e9f73244
Lenny Consuegra <lennyconsuegra@gmail.com>
2011-05-14 00:20:49 +0200

    Parsers + Debut IHM ( main + pick )

--- src/me/manager/mainActivity.java
+++ src/me/manager/mainActivity.java
@@ -1,50 +1,41 @@
 package me.manager;
 
-import java.io.InputStream;
 import java.util.ArrayList;
-
 import object.Input;
 import parser.Parser;
 import android.app.Activity;
-import android.graphics.Color;
-import android.graphics.Rect;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.text.method.PasswordTransformationMethod;
-import android.text.method.TransformationMethod;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup.LayoutParams;
+import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TableLayout;
-import android.widget.TableRow;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class mainActivity extends Activity {
     /** Called when the activity is first created. */
 	
 	private static final String LOG_TAG = "mainActivity";
-	private TableLayout tableLayout;
-	private TableRow tr1;
+	LinearLayout linear;
+	LayoutInflater inflater;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        linear = (LinearLayout) findViewById(R.id.layoutlist); 
+        inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+      
         
-        tableLayout = (TableLayout) findViewById(R.id.mytablelayout);
-        tr1=new TableRow(this);
-        tr1.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT,LayoutParams.FILL_PARENT));
-        tableLayout.addView(tr1);
-        
-        
-        InputStream sourceUrlString = null;
-        //String sourceUrlString = "http://www.google.fr";
+        //InputStream sourceUrlString = null;
+        String sourceUrlString = "http://www.google.fr";
 		
 		try {
 			
-			sourceUrlString=getAssets().open("data/freewifi.htm");
+			//sourceUrlString=getAssets().open("data/freewifi.htm");
 			Parser myparser = new Parser(sourceUrlString);
 			
 			ArrayList<Input> inputsAndroid = new ArrayList<Input>();
@@ -63,6 +54,23 @@ public class mainActivity extends Activity {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
+		//On crée le Listener sur le Bouton
+		OnClickListener ButtonList = new OnClickListener()
+		{
+		 @Override
+		 public void onClick(View actuelView)
+		 {
+		 // On met en place le passage entre les deux activités sur ce Listener
+		 Intent intent = new Intent(mainActivity.this,pickActivity.class);
+		 startActivity(intent);
+		 }
+		};
+		 
+		//On récupere le bouton souhaité et on lui affecte le Listener
+		Button bouton = (Button) findViewById(R.id.buttonList);
+		bouton.setOnClickListener(ButtonList);
+		
     }	
 	
     public boolean isGoogle(ArrayList<Input> inputsAndroid){
@@ -83,35 +91,34 @@ public class mainActivity extends Activity {
     	
     	
     	for(int i=0; i < inputsAndroid.size();i++){
-    		
-    	 	Log.i(LOG_TAG, "Construction d'un :" + inputsAndroid.get(i).getType());
+
     	 	
     		if(inputsAndroid.get(i).getType().equals("text")){
-    						
-    			TextView label = new TextView(this);
-    			label.setText(inputsAndroid.get(i).getName());
-    			tr1.addView(label);
+ 
+    			Log.i(LOG_TAG, "Construction d'un :" + inputsAndroid.get(i).getType());
     			
-    			EditText text=new EditText(this);
-    			tr1.addView(text);
+     		     View text = inflater.inflate(R.layout.text, linear,false);
+     		     TextView label = (TextView) text.findViewById(R.id.textLabel);
+    		     linear.addView(text);
     		}
     		else if(inputsAndroid.get(i).getType().equals("password")){
     			
-    			TextView label = new TextView(this);
-    			label.setText(inputsAndroid.get(i).getName());
-    			tr1.addView(label);
+    			Log.i(LOG_TAG, "Construction d'un :" + inputsAndroid.get(i).getType());
     			
-    			EditText pass=new EditText(this);
-    			tr1.addView(pass);
+    			 View password = inflater.inflate(R.layout.password, linear,false);
+     		     TextView label = (TextView) password.findViewById(R.id.textPassword);
+    		     linear.addView(password);
     		}
     		else if(inputsAndroid.get(i).getType().equals("submit")){
     			
-    			Button submit = new Button(this);
-    			submit.setText(inputsAndroid.get(i).getValue());
-    			tr1.addView(submit);
-
+    			Log.i(LOG_TAG, "Construction d'un: " + inputsAndroid.get(i).getType());
+    			
+    			View submit = inflater.inflate(R.layout.submit, linear,false);
+   		     	linear.addView(submit);
     		}
-    		
+    		else {
+    			Log.i(LOG_TAG, "Type non pris en charge" + inputsAndroid.get(i).getType());
+			}
     	}
     	
     	
